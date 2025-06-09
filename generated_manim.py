d3d4b5e69dd252c63e18c6dcda8e5f67eb1b59ca
ammar-abagumbul <ammar96@connect.hku.hk>
2025-06-09 21:02:28 +0800

    feat: langgraph implementation

--- generated_manim.py
+++ generated_manim.py
@@ -1,81 +1,52 @@
 from manim import *
 
-class TypesOfTriangles(Scene):
+class TypesOfNumbers(Scene):
     def construct(self):
-        title = Text("Types of Triangles", font_size=36)
-        title.to_edge(UP)
+        # Title
+        title = Text("Types of Numbers", font_size=42).to_edge(UP)
         self.play(Write(title))
 
-        # By Sides
-        sides_title = Text("By Sides", font_size=28).to_edge(LEFT).shift(DOWN*0.5)
-        self.play(Write(sides_title))
+        # Circles for different sets
+        real_circle = Circle(radius=3.5, color=YELLOW).shift(LEFT)
+        rational_circle = Circle(radius=2.6, color=BLUE).shift(LEFT*1.4 + DOWN*0.8)
+        irrational_circle = Circle(radius=2.6, color=GREEN).shift(RIGHT*1.2 + DOWN*0.7)
+        integer_circle = Circle(radius=1.5, color=RED).shift(LEFT*2)
+        whole_circle = Circle(radius=0.8, color=PURPLE).shift(LEFT*2.5 + UP*0.5)
+        natural_circle = Circle(radius=0.45, color=ORANGE).shift(LEFT*2.5 + UP*1.15)
 
-        # Scalene triangle
-        scalene = Polygon(
-            [0, 0, 0],
-            [2, 0, 0],
-            [0.5, 1.5, 0],
-            color=BLUE
-        ).shift(LEFT*4 + DOWN*2)
-        scalene_label = Text("Scalene", font_size=24).next_to(scalene, DOWN)
-        # Isosceles triangle
-        isosceles = Polygon(
-            [0, 0, 0],
-            [2, 0, 0],
-            [1, 1.5, 0],
-            color=GREEN
-        ).shift(LEFT*0.5 + DOWN*2)
-        isosceles_label = Text("Isosceles", font_size=24).next_to(isosceles, DOWN)
-        # Equilateral triangle
-        from numpy import array
-        h = (3**0.5)/2*2
-        equilateral = Polygon(
-            [0, 0, 0],
-            [2, 0, 0],
-            [1, h, 0],
-            color=YELLOW
-        ).shift(RIGHT*3 + DOWN*2)
-        equilateral_label = Text("Equilateral", font_size=24).next_to(equilateral, DOWN)
-        self.play(Create(scalene), Write(scalene_label))
-        self.play(Create(isosceles), Write(isosceles_label))
-        self.play(Create(equilateral), Write(equilateral_label))
+        # Labels
+        real_label = Text("Real Numbers", font_size=26, color=YELLOW).next_to(real_circle, UP, buff=0.1)
+        rational_label = Text("Rational Numbers", font_size=22, color=BLUE).next_to(rational_circle, DOWN, buff=0.1)
+        irrational_label = Text("Irrational Numbers", font_size=22, color=GREEN).next_to(irrational_circle, DOWN, buff=0.1)
+        integer_label = Text("Integers", font_size=22, color=RED).next_to(integer_circle, LEFT, buff=0.05)
+        whole_label = Text("Whole Numbers", font_size=18, color=PURPLE).next_to(whole_circle, UP, buff=0.05)
+        natural_label = Text("Natural Numbers", font_size=16, color=ORANGE).next_to(natural_circle, UP, buff=0.05)
 
-        # By Angles
-        angles_title = Text("By Angles", font_size=28).to_edge(LEFT).shift(UP*0.5)
-        self.play(FadeOut(scalene), FadeOut(isosceles), FadeOut(equilateral),
-                  FadeOut(scalene_label), FadeOut(isosceles_label), FadeOut(equilateral_label),
-                  FadeOut(sides_title))
-        self.play(Write(angles_title))
+        # Complex Numbers Around
+        complex_rect = Rectangle(width=10, height=6.5, color=TEAL).shift(DOWN*0.7)
+        complex_label = Text("Complex Numbers", font_size=28, color=TEAL).next_to(complex_rect, UP, buff=0.15)
 
-        # Acute triangle (all angles < 90)
-        acute = Polygon(
-            [0, 0, 0],
-            [2, 0, 0],
-            [1, 1.3, 0],
-            color=PURPLE
-        ).shift(LEFT*4 + UP*1)
-        acute_label = Text("Acute", font_size=24).next_to(acute, DOWN)
-        # Right triangle (one angle = 90)
-        right = Polygon(
-            [0, 0, 0],
-            [2, 0, 0],
-            [0, 1.5, 0],
-            color=RED
-        ).shift(LEFT*0.5 + UP*1)
-        right_label = Text("Right", font_size=24).next_to(right, DOWN)
-        # Small square for right angle
-        right_angle = (
-            Square(0.3, color=WHITE, fill_opacity=1)
-            .move_to([0.2, 0.2, 0])
-            .shift(LEFT * 0.5 + UP * 1)
+        # Number Examples
+        examples = VGroup(
+            Tex(r"\\mathbb{N}: \\ \{1, 2, 3,...\}", color=ORANGE).scale(0.65).next_to(natural_circle, UP*2, buff=0.15),
+            Tex(r"\\mathbb{W}: \\ \{0, 1, 2,...\}", color=PURPLE).scale(0.65).next_to(whole_circle, LEFT*1.5, buff=0.05),
+            Tex(r"\\mathbb{Z}: ...,-2,-1,0,1,2,...", color=RED).scale(0.7).next_to(integer_circle, LEFT*2.2, buff=0.05),
+            Tex(r"\\mathbb{Q}: 1/2, -3/4, 0.25", color=BLUE).scale(0.7).next_to(rational_circle, LEFT*1.7 + DOWN*0.85, buff=0.05),
+            Tex(r"I: \\sqrt{2}, \\pi", color=GREEN).scale(0.7).next_to(irrational_circle, RIGHT*1.6 + DOWN*1, buff=0.05),
+            Tex(r"\\mathbb{R}: \\mathbb{Q} \\cup I", color=YELLOW).scale(0.7).next_to(real_circle, DOWN*2, buff=0.01),
+            Tex(r"\\mathbb{C}: 2+3i", color=TEAL).scale(0.7).next_to(complex_rect, DOWN*3.3, buff=0.05),
         )
-        # Obtuse triangle (one angle > 90)
-        obtuse = Polygon([0, 0, 0], [2, 0, 0], [1.7, 0.8, 0], color=ORANGE).shift(
-            RIGHT * 3 + UP * 1
+
+        # Draw everything
+        self.play(
+            DrawBorderThenFill(complex_rect),
+            Write(complex_label)
         )
-        obtuse_label = Text("Obtuse", font_size=24).next_to(obtuse, DOWN)
-        self.play(Create(acute), Write(acute_label))
-        self.play(Create(right), Write(right_label), FadeIn(right_angle))
-        self.play(Create(obtuse), Write(obtuse_label))
+        self.play(Create(real_circle), Write(real_label))
+        self.play(Create(rational_circle), Write(rational_label))
+        self.play(Create(irrational_circle), Write(irrational_label))
+        self.play(Create(integer_circle), Write(integer_label))
+        self.play(Create(whole_circle), Write(whole_label))
+        self.play(Create(natural_circle), Write(natural_label))
+        self.play(*[Write(ex) for ex in examples])
         self.wait(2)
-

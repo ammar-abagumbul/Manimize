00f93289415548d6b503e3788f76ec3f568c2078
ammar-abagumbul <ammar96@connect.hku.hk>
2025-08-24 10:01:17 +0800

    fix: correct service name for celery-redis connection

--- backend/src/dcelery/celery.py
+++ backend/src/dcelery/celery.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery("manim_tasks", broker='redis://localhost:6379/0')
+app = Celery("manim_tasks", broker='redis://redis:6379/0')
 
 if __name__ == "__main__":
     app.start()
